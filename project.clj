8857b4d91d40de39ad306fa593115a25ce8c6905
Harri Salokorpi <hsalokor@iki.fi>
2013-03-17 19:34:11 +0200

    Update project.clj and dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,33 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.6"]
-                 [compojure "1.1.3"]
-                 [aleph "0.3.0-beta7"]
-                 [midje "1.5-alpha1"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [ring "1.1.8"]
+                 [compojure "1.1.5"]
+                 [http-kit "2.0.0-RC4"]
+                 [midje "1.5.0"]
 
-                 [jayq "0.3.2"]
-                 [crate "0.2.2"]]
-  :plugins [[lein-cljsbuild "0.2.9"]
-            [lein-midje "2.0.3"]]
+                 ; Clojurescript
+                 [jayq "2.3.0"]
+                 [prismatic/dommy "0.0.2"]
+                 [litmus "0.0.3"]]
+  :plugins [[lein-cljsbuild "0.3.1"]
+            [lein-midje "3.0.0"]]
+  :aot [skeleton.main]
   :source-paths ["src/clj"]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:test-commands {"unit" ["mocha-phantomjs" "resources/test.html"]}
+              :builds {:dev {:source-paths ["src/cljs"]
+                             :compiler {:output-to "resources/public/js/skeleton.js"
+                                        :optimizations :whitespace
+                                        :externs ["src/cljs/externs/mocha.js"
+                                                  "src/cljs/externs/chai.js"]
+                                        :pretty-print true}}
+                       :test {:source-paths ["src/cljs"
+                                             "test/cljs/"]
+                              :compiler {:output-to "resources/public/js/tests.js"
+                                         :optimizations :whitespace
+                                         :externs ["src/cljs/externs/mocha.js"
+                                                   "src/cljs/externs/chai.js"]
+                                         :pretty-print true}}}}
   :main skeleton.main)
