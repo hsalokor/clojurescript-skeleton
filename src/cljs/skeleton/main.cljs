a87d7f666e31b4eda4bc99c53098505fc82c3ce5
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2013-03-20 15:35:34 +0200

    Fix infinite recursion on alert call

--- src/cljs/skeleton/main.cljs
+++ src/cljs/skeleton/main.cljs
@@ -1,9 +1,11 @@
 (ns skeleton.main
-  (:use [jayq.core :only [$ on]])
+  (:use [jayq.core :only [$ on]]
+        [jayq.util :only [log]])
   (:use-macros [jayq.macros :only [ready]]))
 
-(defn- alert []
+(defn- show-alert []
+  (log "Alert called")
   (js/alert "Hello world!"))
 
 (ready
-  (on ($ "#lol") "click" alert))
+  (on ($ "#divvi") :click show-alert))
